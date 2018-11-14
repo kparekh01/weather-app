e05cb1bdf4fda90d3a1f836f7468b6ef242f31e4
Kairav Parekh <kparekh01@gmail.com>
2018-11-14 14:53:08 -0500

    finished demo-app requirements and in the process of testing

---
+++ app/controllers/weathers_controller.rb
@@ -0,0 +1,25 @@
+class WeathersController < ApplicationController
+
+  def new
+  end
+
+  def show
+  end
+
+  def post
+    zipcode = params[:zipcode]
+    details = Unirest.get("https://www.zipcodeapi.com/rest/#{Rails.application.credentials.zip_code_api}/info.json/#{zipcode}/degrees").body
+
+    @location = "#{details["city"]}, #{details["state"]}"
+    lat = details["lat"]
+    lng = details["lng"]
+
+    weather = Unirest.get("https://api.darksky.net/forecast/#{Rails.application.credentials.dark_sky_api}/#{lat},#{lng}").body
+    daily_temps = weather["hourly"]["data"].first(24).map{|hash| hash["temperature"].to_i}.sort
+
+    @current_temp = weather["currently"]["temperature"] #current_temp
+    @daily_high = daily_temps.last
+    @daily_low = daily_temps.first
+    render 'show.html.erb'
+  end
+end
